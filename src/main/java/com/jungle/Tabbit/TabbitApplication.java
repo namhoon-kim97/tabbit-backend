86b2d7a1e2de100672a7c743f49acb4bfb9f8d2e
youngmanpark <py980627@gmail.com>
2024-06-17 14:13:09 +0900

    init commit

---
+++ src/main/java/com/jungle/Tabbit/TabbitApplication.java
@@ -0,0 +1,13 @@
+package com.jungle.Tabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jungle/Tabbit/TabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungle.Tabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
