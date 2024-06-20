bfc077f2eeab1eb3f072df33f197f2709d38a28f
namhoon-kim97 <kskcmh5723@gmail.com>
2024-06-21 01:01:50 +0900

    refactor: waiting controller에 nfcId param에서 requestBody로 보내게 수정

--- src/main/java/com/jungle/Tabbit/domain/waiting/controller/WaitingController.java
+++ src/main/java/com/jungle/Tabbit/domain/waiting/controller/WaitingController.java
@@ -6,17 +6,20 @@ import com.jungle.Tabbit.domain.waiting.service.WaitingService;
 import com.jungle.Tabbit.global.model.CommonResponse;
 import com.jungle.Tabbit.global.model.ResponseStatus;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/waitings")
 @RequiredArgsConstructor
 public class WaitingController {
-    private WaitingService waitingService;
+    private final WaitingService waitingService;
 
-    @PostMapping("/{nfcId}")
-    public CommonResponse<?> registerWaiting(@RequestBody WaitingRequestCreateDto requestDto, @PathVariable("nfcId") String nfcId) {
-        WaitingResponseDto responseDto = waitingService.registerWaiting(requestDto, nfcId);
+    @PostMapping
+    public CommonResponse<?> registerWaiting(@RequestBody WaitingRequestCreateDto requestDto) {
+        WaitingResponseDto responseDto = waitingService.registerWaiting(requestDto);
         return CommonResponse.success(ResponseStatus.SUCCESS_CREATE, responseDto);
     }
 }
