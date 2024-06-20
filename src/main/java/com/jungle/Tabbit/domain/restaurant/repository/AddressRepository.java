689413ae852d42f40e534be41d7a192ca090c4ee
jihyun <cat2998@naver.com>
2024-06-21 01:06:28 +0900

    chore: AddressRepository 삭제

--- src/main/java/com/jungle/Tabbit/domain/restaurant/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jungle.Tabbit.domain.restaurant.repository;
-
-import com.jungle.Tabbit.domain.restaurant.entity.Address;
-import org.springframework.data.repository.Repository;
-
-public interface AddressRepository extends Repository<Address, Long> {
-    Address save(Address address);
-}
