bd1d095a2ca1fb7782ef20d0504e0aa8df64df8c
dylanYim <dongkyun94@gmail.com>
2024-06-20 17:38:26 +0900

    feat: nickname 룰 변경

--- src/main/java/com/jungle/Tabbit/domain/member/dto/MemberJoinRequestDto.java
+++ src/main/java/com/jungle/Tabbit/domain/member/dto/MemberJoinRequestDto.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class MemberJoinRequestDto {
     @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "Username must be between 4 and 10 characters long and contain only lowercase letters and digits.")
     private String username;
-//    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "Nickname must be between 4 and 10 characters long and contain only lowercase letters and digits.") validation rule 같이 생각해줘요
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]+${2,10}", message = "Nickname must be between 2 and 10 characters long and contain only letters and digits.")
     private String nickname;
     @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()]{8,15}$", message = "Password must be between 8 and 15 characters long and contain only letters and digits.")
     private String password;
