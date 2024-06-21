3a6fb8dab754100bcf8aefb9640298411fa63f89
jihyun <cat2998@naver.com>
2024-06-21 22:53:11 +0900

    chore: RestaurantResponseDto 항목 수정

--- src/main/java/com/jungle/Tabbit/domain/restaurant/dto/RestaurantResponseDto.java
+++ src/main/java/com/jungle/Tabbit/domain/restaurant/dto/RestaurantResponseDto.java
@@ -1,5 +1,6 @@
 package com.jungle.Tabbit.domain.restaurant.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jungle.Tabbit.domain.restaurant.entity.Restaurant;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,22 +12,23 @@ import java.math.BigDecimal;
 public class RestaurantResponseDto {
 
     private Long restaurantId;
+    private String name; // 가게 이름
+    private String category_name; // 카테고리 이름
+    private String summary_address; // 요약 주소
+    private BigDecimal latitude; // 위도
+    private BigDecimal longitude; // 경도
+    @JsonProperty("is_earned_stamp")
+    private boolean is_earned_stamp; // 스탬프 획득 유무
 
-    private String name;
-
-    private BigDecimal latitude;
-
-    private BigDecimal longitude;
-
-    private boolean is_stamp;
-
-    public static RestaurantResponseDto of(Restaurant restaurant, boolean is_stamp) {
+    public static RestaurantResponseDto of(Restaurant restaurant, boolean is_earned_stamp) {
         return RestaurantResponseDto.builder()
                 .restaurantId(restaurant.getRestaurantId())
                 .name(restaurant.getName())
+                .category_name(restaurant.getCategory().getCategoryName())
+                .summary_address(restaurant.getAddress().getSido() + " " + restaurant.getAddress().getSigungu() + " " + restaurant.getAddress().getEupmyeondong())
                 .latitude(restaurant.getLatitude())
                 .longitude(restaurant.getLongitude())
-                .is_stamp(is_stamp)
+                .is_earned_stamp(is_earned_stamp)
                 .build();
     }
 }
