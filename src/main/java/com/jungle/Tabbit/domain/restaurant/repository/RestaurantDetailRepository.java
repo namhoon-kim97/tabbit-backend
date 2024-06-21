007dfc9d11d43df196035d0c150217c08062e01a
jihyun <cat2998@naver.com>
2024-06-21 13:15:49 +0900

    chore: restaurantDetailRepository 삭제

--- src/main/java/com/jungle/Tabbit/domain/restaurant/repository/RestaurantDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jungle.Tabbit.domain.restaurant.repository;
-
-import com.jungle.Tabbit.domain.restaurant.entity.RestaurantDetail;
-import org.springframework.data.repository.Repository;
-
-public interface RestaurantDetailRepository extends Repository<RestaurantDetail, Long> {
-    RestaurantDetail save(RestaurantDetail restaurantDetail);
-}
