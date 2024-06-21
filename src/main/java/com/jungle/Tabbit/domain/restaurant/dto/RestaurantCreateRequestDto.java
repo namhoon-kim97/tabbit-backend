754f0b391d483f626c25e621acc6bc65b737db82
jihyun <cat2998@naver.com>
2024-06-21 22:52:39 +0900

    chore: RestaurantCreateRequestDto 항목 추가

--- src/main/java/com/jungle/Tabbit/domain/restaurant/dto/RestaurantCreateRequestDto.java
+++ src/main/java/com/jungle/Tabbit/domain/restaurant/dto/RestaurantCreateRequestDto.java
@@ -10,9 +10,12 @@ import java.math.BigDecimal;
 public class RestaurantCreateRequestDto {
     private String place_name;  // 업체명
     private String categoryCd;  // 카테고리 코드
-    private String address_name; // 전체 지번 주소
-    private String road_address_name; // 전체 도로명 주소
+    private String address_name; // 지번 주소
+    private String road_address_name; // 도로명 주소
     private String detail_address; // 상세 주소
+    private String sido; // 시도
+    private String sigungu; // 시군구
+    private String eupmyeondong; // 읍면동
     private BigDecimal longitude; // 경도
     private BigDecimal latitude; // 위도
     private Long estimatedTimePerTeam; // 팀당 예상 대기 시간
