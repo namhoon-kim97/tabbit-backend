86c19b7cc8c4eb8c99eb3435fcfdbe18f8577bac
jihyun <cat2998@naver.com>
2024-06-23 23:21:41 +0900

    chore: swagger schema 설정

--- src/main/java/com/jungle/Tabbit/domain/stampBadge/dto/StampResponseDto.java
+++ src/main/java/com/jungle/Tabbit/domain/stampBadge/dto/StampResponseDto.java
@@ -1,6 +1,7 @@
 package com.jungle.Tabbit.domain.stampBadge.dto;
 
 import com.jungle.Tabbit.domain.restaurant.dto.RestaurantResponseDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,11 +9,16 @@ import java.util.List;
 
 @Getter
 @Builder
+@Schema(description = "스탬프 응답 DTO")
 public class StampResponseDto {
-    private String sidoName; // 시도명
-    private Long totalStampCount; // 해당 지역 총 스탬프 수
-    private Long earnedStampCount; // 해당 지역 획득 스탬프 수
-    private List<RestaurantResponseDto> restaurantList; // 해당 지역 맛집 리스트
+    @Schema(description = "시도명", example = "서울특별시")
+    private String sidoName;
+    @Schema(description = "해당 지역 총 스탬프 수", example = "10")
+    private Long totalStampCount;
+    @Schema(description = "해당 지역 획득 스탬프 수", example = "5")
+    private Long earnedStampCount;
+    @Schema(description = "해당 지역 맛집 리스트")
+    private List<RestaurantResponseDto> restaurantList;
 
     public static StampResponseDto of(String sidoName, Long totalStampCount, Long earnedStampCount, List<RestaurantResponseDto> restaurantList) {
         return StampResponseDto.builder()
--- src/main/java/com/jungle/Tabbit/domain/stampBadge/dto/StampResponseListDto.java
+++ src/main/java/com/jungle/Tabbit/domain/stampBadge/dto/StampResponseListDto.java
@@ -1,5 +1,6 @@
 package com.jungle.Tabbit.domain.stampBadge.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,6 +8,8 @@ import java.util.List;
 
 @Getter
 @Builder
+@Schema(description = "스탬프 응답 리스트 DTO")
 public class StampResponseListDto {
+    @Schema(description = "스탬프 응답 리스트")
     private List<StampResponseDto> stampResponseList;
 }
