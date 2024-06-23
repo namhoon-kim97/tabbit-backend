00bc5e5c2d23c34f25579e6ab2d1cf27bd08df87
jihyun <cat2998@naver.com>
2024-06-23 22:44:08 +0900

    feat: restaurant join address 조회

--- src/main/java/com/jungle/Tabbit/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/jungle/Tabbit/domain/restaurant/repository/RestaurantRepository.java
@@ -2,6 +2,7 @@ package com.jungle.Tabbit.domain.restaurant.repository;
 
 import com.jungle.Tabbit.domain.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
@@ -10,8 +11,12 @@ import java.util.Optional;
 public interface RestaurantRepository extends Repository<Restaurant, Long> {
     List<Restaurant> findAll();
 
+    @EntityGraph(attributePaths = {"address"})
+    @Query("SELECT r FROM Restaurant r")
+    List<Restaurant> findAllWithAddress();
+
     void save(Restaurant restaurant);
 
-    @EntityGraph(attributePaths = {"category", "address"})
+    @EntityGraph(attributePaths = {"category", "address", "restaurantDetail"})
     Optional<Restaurant> findByRestaurantId(Long id);
 }
