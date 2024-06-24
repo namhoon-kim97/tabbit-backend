c6aa2344a7b233353d24b52b9304eacff740f118
youngmanpark <py980627@gmail.com>
2024-06-25 01:58:25 +0900

    feat: FcmRequestDto 생성자 추가

--- src/main/java/com/jungle/Tabbit/domain/fcm/dto/FcmRequestDto.java
+++ src/main/java/com/jungle/Tabbit/domain/fcm/dto/FcmRequestDto.java
@@ -7,9 +7,13 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FcmRequestDto {
     private String token;
-
     private String title;
-
     private String body;
 
+    @Builder
+    public FcmRequestDto(String token, String title, String body) {
+        this.token = token;
+        this.title = title;
+        this.body = body;
+    }
 }
