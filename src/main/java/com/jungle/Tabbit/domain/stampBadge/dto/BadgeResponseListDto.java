50ac7f110852c975156d6685346bd3695beb9d35
jihyun <cat2998@naver.com>
2024-06-26 00:26:31 +0900

    feat: badge, stamp responseDto에 총 갯수, 획득 갯수 추가

--- src/main/java/com/jungle/Tabbit/domain/stampBadge/dto/BadgeResponseListDto.java
+++ src/main/java/com/jungle/Tabbit/domain/stampBadge/dto/BadgeResponseListDto.java
@@ -10,6 +10,18 @@ import java.util.List;
 @Builder
 @Schema(description = "칭호 응답 리스트 DTO")
 public class BadgeResponseListDto {
+    @Schema(description = "총 칭호 수", example = "5")
+    private Long totalBadgeCount;
+    @Schema(description = "획득 칭호 수", example = "1")
+    private Long earnedBadgeCount;
     @Schema(description = "칭호 응답 리스트")
     private List<BadgeResponseDto> badgeResponseList;
+
+    public static BadgeResponseListDto of(Long totalBadgeCount, Long earnedBadgeCount, List<BadgeResponseDto> badgeResponseList) {
+        return BadgeResponseListDto.builder()
+                .totalBadgeCount(totalBadgeCount)
+                .earnedBadgeCount(earnedBadgeCount)
+                .badgeResponseList(badgeResponseList)
+                .build();
+    }
 }
