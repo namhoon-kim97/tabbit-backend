adeabead83b9c058c5484f7b821e9ce11f278eab
jihyun <cat2998@naver.com>
2024-06-25 20:50:21 +0900

    chore: controller apiresponse 수정

--- src/main/java/com/jungle/Tabbit/domain/stampBadge/controller/BadgeController.java
+++ src/main/java/com/jungle/Tabbit/domain/stampBadge/controller/BadgeController.java
@@ -6,8 +6,9 @@ import com.jungle.Tabbit.global.config.security.CustomUserDetails;
 import com.jungle.Tabbit.global.model.CommonResponse;
 import com.jungle.Tabbit.global.model.ResponseStatus;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,9 +26,7 @@ public class BadgeController {
 
     @GetMapping
     @Operation(summary = "칭호 전체 조회", description = "사용자의 모든 칭호를 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "칭호 조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = BadgeResponseListDto.class))),
-    })
+    @ApiResponse(responseCode = "200", description = "칭호 조회 성공", content = @Content(schema = @Schema(implementation = BadgeResponseListDto.class)))
     public CommonResponse<?> getBadgeAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return CommonResponse.success(ResponseStatus.SUCCESS_OK, badgeService.getBadgeAll(userDetails.getUsername()));
     }
