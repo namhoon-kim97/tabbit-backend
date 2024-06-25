33a0e80877af8bf973b8a02b47dce8d6607f2c4d
youngmanpark <py980627@gmail.com>
2024-06-26 02:31:11 +0900

    feat: getUserId 추가

--- src/main/java/com/jungle/Tabbit/global/config/security/CustomUserDetails.java
+++ src/main/java/com/jungle/Tabbit/global/config/security/CustomUserDetails.java
@@ -54,4 +54,8 @@ public class CustomUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getUserId() {
+        return member.getMemberId();
+    }
 }
