b956f1984496aa232fddd64b80a187c4ebcd6159
namhoon-kim97 <kskcmh5723@gmail.com>
2024-07-03 21:12:30 +0900

    style: 필요없는 중복 어노테이션 삭제

--- src/main/java/com/jungle/Tabbit/domain/stampBadge/controller/StampController.java
+++ src/main/java/com/jungle/Tabbit/domain/stampBadge/controller/StampController.java
@@ -7,7 +7,6 @@ import com.jungle.Tabbit.global.model.CommonResponse;
 import com.jungle.Tabbit.global.model.ResponseStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -26,18 +25,14 @@ public class StampController {
 
     @GetMapping
     @Operation(summary = "스탬프 전체 조회", description = "사용자의 모든 스탬프를 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "스탬프 조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = StampResponseListDto.class))),
-    })
+    @ApiResponse(responseCode = "200", description = "스탬프 조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = StampResponseListDto.class)))
     public CommonResponse<?> getStampAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return CommonResponse.success(ResponseStatus.SUCCESS_OK, stampService.getStampAll(userDetails.getUsername()));
     }
 
     @GetMapping("/user")
     @Operation(summary = "유저의 모든 스탬프 조회", description = "특정 유저가 가진 모든 스탬프를 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "유저의 모든 스탬프 조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = StampResponseListDto.class))),
-    })
+    @ApiResponse(responseCode = "200", description = "유저의 모든 스탬프 조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = StampResponseListDto.class)))
     public CommonResponse<?> getStampAllByUser(@RequestParam String username) {
         return CommonResponse.success(ResponseStatus.SUCCESS_OK, stampService.getStampAll(username));
     }
