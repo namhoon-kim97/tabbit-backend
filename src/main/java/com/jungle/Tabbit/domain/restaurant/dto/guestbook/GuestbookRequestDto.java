522a111145f62c273a65eb5b4d0e562dd133f896
jihyun <cat2998@naver.com>
2024-07-03 23:26:23 +0900

    refactor: setter 삭제

--- src/main/java/com/jungle/Tabbit/domain/restaurant/dto/guestbook/GuestbookRequestDto.java
+++ src/main/java/com/jungle/Tabbit/domain/restaurant/dto/guestbook/GuestbookRequestDto.java
@@ -3,12 +3,10 @@ package com.jungle.Tabbit.domain.restaurant.dto.guestbook;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor
-@Setter
 @Schema(description = "방명록 생성 요청 DTO")
 public class GuestbookRequestDto {
     @Schema(description = "방명록 내용")
