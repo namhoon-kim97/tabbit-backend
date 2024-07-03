dc092cfaac4fc24b6632faf77e10f26f76ed16fd
namhoon-kim97 <kskcmh5723@gmail.com>
2024-07-03 20:27:14 +0900

    feat: 특정 유저의 모든 스탬프 조회 기능 구현

--- src/main/java/com/jungle/Tabbit/domain/stampBadge/controller/StampController.java
+++ src/main/java/com/jungle/Tabbit/domain/stampBadge/controller/StampController.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -30,5 +31,14 @@ public class StampController {
     })
     public CommonResponse<?> getStampAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
         return CommonResponse.success(ResponseStatus.SUCCESS_OK, stampService.getStampAll(userDetails.getUsername()));
+    }
+
+    @GetMapping("/user")
+    @Operation(summary = "유저의 모든 스탬프 조회", description = "특정 유저가 가진 모든 스탬프를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저의 모든 스탬프 조회 성공", content = @io.swagger.v3.oas.annotations.media.Content(schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = StampResponseListDto.class))),
+    })
+    public CommonResponse<?> getStampAllByUser(@RequestParam String username) {
+        return CommonResponse.success(ResponseStatus.SUCCESS_OK, stampService.getStampAll(username));
     }
 }
