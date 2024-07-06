7584e87838442bd9b4cd633ec925c83f9802bc4a
jihyun <cat2998@naver.com>
2024-07-06 20:47:11 +0900

    feat: menucategory request, response dto 생성

--- src/main/java/com/jungle/Tabbit/domain/order/repository/MenuCategoryRepository.java
+++ src/main/java/com/jungle/Tabbit/domain/order/repository/MenuCategoryRepository.java
@@ -3,5 +3,13 @@ package com.jungle.Tabbit.domain.order.repository;
 import com.jungle.Tabbit.domain.order.entity.MenuCategory;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MenuCategoryRepository extends Repository<MenuCategory, Long> {
+    List<MenuCategory> findAllByRestaurant_RestaurantId(Long restaurantId);
+
+    void save(MenuCategory menuCategory);
+
+    Optional<MenuCategory> findByCategoryId(Long categoryId);
 }
