441afc5421446caedd18a000c74e754a742c69b7
jihyun <cat2998@naver.com>
2024-07-06 20:47:27 +0900

    feat: menu request, response dto 생성

---
+++ src/main/java/com/jungle/Tabbit/domain/order/dto/menu/MenuResponseDto.java
@@ -0,0 +1,36 @@
+package com.jungle.Tabbit.domain.order.dto.menu;
+
+import com.jungle.Tabbit.domain.order.entity.Menu;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "메뉴 응답 DTO")
+public class MenuResponseDto {
+
+    @Schema(description = "메뉴ID")
+    private Long menuId;
+    @Schema(description = "메뉴명")
+    private String name;
+    @Schema(description = "가격")
+    private Long price;
+    @Schema(description = "설명")
+    private String description;
+    @Schema(description = "이미지 경로")
+    private String imageUrl;
+    @Schema(description = "카테고리명")
+    private String categoryName;
+
+    public static MenuResponseDto of(Menu menu) {
+        return MenuResponseDto.builder()
+                .menuId(menu.getMenuId())
+                .name(menu.getName())
+                .price(menu.getPrice())
+                .description(menu.getDescription())
+                .imageUrl(menu.getImageUrl())
+                .categoryName(menu.getCategory().getCategoryName())
+                .build();
+    }
+}
