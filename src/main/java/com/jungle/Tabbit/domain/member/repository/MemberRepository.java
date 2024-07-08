a091a987ecf123f8e51cf820993616e71e7bac44
youngmanpark <py980627@gmail.com>
2024-07-08 21:02:16 +0900

    feat: soft delete 를 위한 쿼리 추가

--- src/main/java/com/jungle/Tabbit/domain/member/repository/MemberRepository.java
+++ src/main/java/com/jungle/Tabbit/domain/member/repository/MemberRepository.java
@@ -1,13 +1,17 @@
 package com.jungle.Tabbit.domain.member.repository;
 
 import com.jungle.Tabbit.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends Repository<Member, Long> {
     Member save(Member member);
-    Optional<Member> findMemberByUsername(String username);
-    Optional<Member> findMemberByMemberId(Long memberId);
+    @Query("SELECT m FROM member m WHERE m.username = :username AND m.isDeleted = false")
+    Optional<Member> findMemberByUsername(@Param("username") String username);
+    @Query("SELECT m FROM member m WHERE m.memberId = :memberId AND m.isDeleted = false")
+    Optional<Member> findMemberByMemberId(@Param("memberId") Long memberId);
 
 }
