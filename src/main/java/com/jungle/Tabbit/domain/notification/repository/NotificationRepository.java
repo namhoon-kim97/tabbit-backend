4154a9bda0df8f7a7e4feee2c7f920599334c464
youngmanpark <py980627@gmail.com>
2024-07-10 21:20:04 +0900

    fix: 알림 리스트 역순 조회

--- src/main/java/com/jungle/Tabbit/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/jungle/Tabbit/domain/notification/repository/NotificationRepository.java
@@ -11,12 +11,13 @@ public interface NotificationRepository extends Repository<Notification, Long> {
     void save(Notification notification);
 
     List<Notification> findAllByMemberAndDataOnlyFalse(Member member);
-
+    List<Notification> findAllByMemberAndDataOnlyFalseOrderByCreatedAtDesc(Member member);
     Optional<Notification> findNotificationByNotificationId(Long notificationId);
 
     void deleteAllByMember(Member member);
 
     Optional<Notification> findNotificationByNotificationIdAndMember(Long notificationId, Member member);
+
 
     void delete(Notification notification);
 }
