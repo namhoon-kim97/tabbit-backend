23b5ec149dee9e07d1f58d4bc3a86bc25e8d8223
namhoon-kim97 <kskcmh5723@gmail.com>
2024-07-16 16:32:10 +0900

    fix: format에 맞지 않은 validationHandler 주석

--- src/main/java/com/jungle/Tabbit/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jungle/Tabbit/global/handler/GlobalExceptionHandler.java
@@ -3,11 +3,16 @@ package com.jungle.Tabbit.global.handler;
 
 import com.jungle.Tabbit.global.exception.BusinessLogicException;
 import com.jungle.Tabbit.global.model.CommonResponse;
+import com.jungle.Tabbit.global.model.ResponseErrorFormat;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 //전역적으로 예외 처리를 담당하는 클래스
 @RestControllerAdvice
@@ -29,21 +34,21 @@ public class GlobalExceptionHandler {
 
     }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public CommonResponse<?> handleValidationExceptions(MethodArgumentNotValidException e, HttpServletResponse response) {
-//        log.warn(e.getClass().getName(), e);
-//        response.setStatus(HttpStatus.BAD_REQUEST.value());
-//
-//        List<ResponseErrorFormat.ValidationException> validationErrors = e.getBindingResult().getFieldErrors().stream()
-//                .map(ResponseErrorFormat.ValidationException::of)
-//                .collect(Collectors.toList());
-//
-//        ResponseErrorFormat errorFormat = ResponseErrorFormat.builder()
-//                .message("Validation failed")
-//                .statusCode(HttpStatus.BAD_REQUEST)
-//                .validationExceptions(validationErrors)
-//                .build();
-//
-//        return CommonResponse.fail(HttpStatus.BAD_REQUEST, errorFormat);
-//    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public CommonResponse<?> handleValidationExceptions(MethodArgumentNotValidException e, HttpServletResponse response) {
+        log.warn(e.getClass().getName(), e);
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        List<ResponseErrorFormat.ValidationException> validationErrors = e.getBindingResult().getFieldErrors().stream()
+                .map(ResponseErrorFormat.ValidationException::of)
+                .collect(Collectors.toList());
+
+        ResponseErrorFormat errorFormat = ResponseErrorFormat.builder()
+                .message("회원가입 Validation이 실패하였습니다.")
+                .statusCode(HttpStatus.BAD_REQUEST)
+                .validationExceptions(validationErrors)
+                .build();
+
+        return CommonResponse.fail(HttpStatus.BAD_REQUEST, errorFormat);
+    }
 }
