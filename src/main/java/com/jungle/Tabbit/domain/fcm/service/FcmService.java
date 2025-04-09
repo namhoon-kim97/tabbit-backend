61ff9fd88e7dc107f2303b6bf5f0cf5cc70a86b0
namhoon-kim97 <kskcmh5723@gmail.com>
2025-04-09 22:37:40 +0900

    chore: yml 실행 시 경로 변경 및 firebase json파일 경로 복원

--- src/main/java/com/jungle/Tabbit/domain/fcm/service/FcmService.java
+++ src/main/java/com/jungle/Tabbit/domain/fcm/service/FcmService.java
@@ -7,7 +7,6 @@ import com.jungle.Tabbit.domain.fcm.dto.FcmRequestDto;
 import com.jungle.Tabbit.domain.fcm.dto.FcmResponseDto;
 import com.jungle.Tabbit.global.exception.NotFoundException;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import java.io.FileInputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
@@ -54,12 +53,10 @@ public class FcmService {
 
     private String getAccessToken() {
         try {
-            //String firebaseConfigPath = "firebase/tabbit-69517-firebase-adminsdk-fbsvc-91f92182cc.json";
-            String firebaseConfigPath = "/home/ubuntu/tabbit-backend/tabbit-69517-firebase-adminsdk-fbsvc-91f92182cc.json";
+            String firebaseConfigPath = "firebase/tabbit-69517-firebase-adminsdk-fbsvc-91f92182cc.json";
 
             GoogleCredentials googleCredentials = GoogleCredentials
-                    //.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
-                    .fromStream(new FileInputStream(firebaseConfigPath))
+                    .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
                     .createScoped(List.of("https://www.googleapis.com/auth/firebase.messaging"));
 
             googleCredentials.refresh();
