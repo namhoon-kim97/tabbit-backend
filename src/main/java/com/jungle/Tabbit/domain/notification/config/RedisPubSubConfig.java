259dc50bf862d6d7294ed5a8a878bf9e7a0a4ac1
namhoon-kim97 <kskcmh5723@gmail.com>
2025-04-13 19:08:41 +0900

    fix: redisConfig dto 직렬화 설정 추가

--- src/main/java/com/jungle/Tabbit/domain/notification/config/RedisPubSubConfig.java
+++ src/main/java/com/jungle/Tabbit/domain/notification/config/RedisPubSubConfig.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -32,6 +34,15 @@ public class RedisPubSubConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // Key는 String 직렬화
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value는 JSON 직렬화
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
     }
 }
