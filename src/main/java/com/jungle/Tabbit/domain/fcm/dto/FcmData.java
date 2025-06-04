4d8541127c960eb5e766d3f56098664b90a73890
namhoon-kim97 <kskcmh5723@gmail.com>
2025-06-04 22:17:16 +0900

    fix(notification): FcmData에 Redis내부 객체 생성 시 필요한 기본생성자 추가

--- src/main/java/com/jungle/Tabbit/domain/fcm/dto/FcmData.java
+++ src/main/java/com/jungle/Tabbit/domain/fcm/dto/FcmData.java
@@ -3,10 +3,12 @@ package com.jungle.Tabbit.domain.fcm.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class FcmData {
     private String target;
     private String messageType;
